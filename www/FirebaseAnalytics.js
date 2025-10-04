f07814e48b507cac3dcd4d0a7acc2a1f59322349
Schlonki123 <steffen.von.thienen@gmail.com>
2025-10-05 00:34:48 +0200

    Update FirebaseAnalytics.js

--- www/FirebaseAnalytics.js
+++ www/FirebaseAnalytics.js
@@ -19,6 +19,23 @@ function(name, params) {
     });
 };
 
+exports.AnalyticsConsentMode = {
+    ANALYTICS_STORAGE: "ANALYTICS_STORAGE",
+    AD_STORAGE: "AD_STORAGE",
+    AD_USER_DATA: "AD_USER_DATA",
+    AD_PERSONALIZATION: "AD_PERSONALIZATION"
+};
+
+exports.AnalyticsConsentStatus = {
+    GRANTED: "GRANTED",
+    DENIED: "DENIED"
+};
+
+exports.setAnalyticsConsentMode = function(consent, success, error) {
+    exec(success, error, "FirebasePlugin", "setAnalyticsConsentMode", [consent]);
+};
+
+
 exports.setUserId =
 /**
  * Sets the user ID property. This feature must be used in accordance with Google's Privacy Policy.
