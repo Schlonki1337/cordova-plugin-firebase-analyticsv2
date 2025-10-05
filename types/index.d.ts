d268649070165b8cf01108024881925f69678f6e
Schlonki123 <steffen.von.thienen@gmail.com>
2025-10-05 11:41:57 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,18 +5,3 @@ interface CordovaPlugins {
 interface FirebasePlugins {
     analytics: typeof import("./FirebaseAnalytics");
 }
-
-export interface IChannelOptions {
-    id: string
-    name?: string
-    description?: string
-    sound?: string
-    vibration?: boolean | number[]
-    light?: boolean
-    lightColor?: string
-    importance?: 0 | 1 | 2 | 3 | 4
-    badge?: boolean
-    visibility?: -1 | 0 | 1
-    usage?: number
-    streamType?: number
-}
